c849ee4c0bbacedb59b7b78a54e838a445bf28d6
eddie naff <eddienaff@eddienaff.com>
2019-05-04 16:38:02 -0700

    Create separate icon package and make colors adaptive

--- modules/traveling/TravelingMain.tsx
+++ modules/traveling/TravelingMain.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Box, Flex, Text } from "rebass";
+import posed from "react-pose";
 
 import Viewbox from "./Viewbox";
 import { GetAllHotelDataComponent } from "../../generated/apolloComponents";
 import { Button } from "../../components/Button/Button";
+import { Filter } from "./Filter/Filter";
 
 type MyProps = {
   data: any;
@@ -11,53 +13,74 @@ type MyProps = {
 
 type MyState = {
   fakeState: string;
+  open: boolean;
 };
+
+const FlexSpecial = posed(Flex)({
+  closed: { height: "0" },
+  open: { height: "auto" }
+});
 
 export default class TravelingMain extends React.Component<MyProps, MyState> {
   state = {
-    fakeState: ""
+    fakeState: "",
+    open: false
   };
   render() {
     const thisIsMe = this.props.data.data.me;
+    const { open } = this.state;
     return (
-      <Flex
-        bg="transparent"
-        color="text"
-        width={[1, 0.6]}
-        flexDirection="column"
-      >
-        <Flex alignItems="center" color="text">
-          <Box width={1}>
-            <Text fontWeight="600" fontSize={[4, 5]} color="text">
-              Featured
-            </Text>
-          </Box>
-
-          <Flex>
-            <Button height="165px" label="Filters" />
-          </Flex>
-        </Flex>
-        <GetAllHotelDataComponent
-          variables={{
-            data: {
-              skip: 0,
-              take: 15
-            }
-          }}
+      <>
+        <Flex
+          bg="transparent"
+          color="text"
+          width={[1, 0.6]}
+          flexDirection="column"
         >
-          {getAllHotel =>
-            getAllHotel && getAllHotel.data ? (
-              <>
-                <Text>{getAllHotel.data.getAllHotel.length} spots</Text>
+          <Flex alignItems="center" color="text">
+            <Box width={1}>
+              <Text fontWeight="600" fontSize={[4, 5]} color="text">
+                Featured
+              </Text>
+            </Box>
 
-                <Viewbox requestor={thisIsMe} data={getAllHotel.data} />
-              </>
-            ) : (
-              <span>"loading..."</span>
-            )
-          }
-        </GetAllHotelDataComponent>
-      </Flex>
+            <Flex>
+              <Button
+                onClick={() =>
+                  this.setState(prevState => ({
+                    open: !prevState.open
+                  }))
+                }
+                height="165px"
+                label="Filters"
+              />
+            </Flex>
+          </Flex>
+          <FlexSpecial width={1} pose={open ? "open" : "closed"}>
+            <Filter />
+          </FlexSpecial>
+          <GetAllHotelDataComponent
+            variables={{
+              data: {
+                skip: 0,
+                take: 15
+              }
+            }}
+          >
+            {getAllHotel =>
+              getAllHotel && getAllHotel.data ? (
+                <>
+                  <Text>{getAllHotel.data.getAllHotel.length} spots</Text>
+
+                  <Viewbox requestor={thisIsMe} data={getAllHotel.data} />
+                </>
+              ) : (
+                <span>"loading..."</span>
+              )
+            }
+          </GetAllHotelDataComponent>
+        </Flex>
+      </>
     );
   }
 }
